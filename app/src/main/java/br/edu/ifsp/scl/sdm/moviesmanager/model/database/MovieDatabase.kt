6d8cb28642a5ed246511455718687fc37b49a9cf
Fernando <suporteiros@gmail.com>
2024-10-28 22:59:31 -0300

    Layout Ready

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/moviesmanager/model/database/MovieDatabase.kt
@@ -0,0 +1,15 @@
+package br.edu.ifsp.scl.sdm.moviesmanager.model.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import br.edu.ifsp.scl.sdm.moviesmanager.model.dao.MovieDao
+import br.edu.ifsp.scl.sdm.moviesmanager.model.entity.MovieProperties
+
+@Database(entities = [MovieProperties::class], version = 1)
+abstract class MovieDatabase : RoomDatabase() {
+    companion object {
+        const val MOVIE_DATABASE = "movieDatabase"
+    }
+
+    abstract fun getMovieDao(): MovieDao
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/moviesmanager/model/entity/MovieProperties.kt
@@ -0,0 +1,20 @@
+package br.edu.ifsp.scl.sdm.moviesmanager.model.entity
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import br.edu.ifsp.scl.sdm.moviesmanager.model.dao.MovieDao
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity(tableName = MovieDao.MOVIE_TABLE)
+data class MovieProperties(
+    @PrimaryKey
+    var name: String, // Nome único do filme
+    var year: Int, // Ano de lançamento do filme
+    var studio: String, // Produtora ou estúdio
+    var duration: Int, // Tempo de duração em minutos
+    var watched: Boolean = false, // Flag para indicar se foi assistido
+    var rating: Float? = null, // Nota do usuário (0-10)
+    var genre: String // Gênero do filme
+) : Parcelable
